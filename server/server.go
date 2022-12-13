d24400c55e874c2e2b593b10b62724454476a856
beyrakIn <root@kali>
2022-12-13 18:43:09 +0400

    Now reads configuration from the file.

--- server/server.go
+++ server/server.go
@@ -1,23 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/labstack/echo/v4"
+	"io/ioutil"
+	"os"
 	. "schrader/server/middlewares"
 	. "schrader/server/routes"
 )
 
 var (
-	addr = ":443"
-	cert = "cert/cert.crt"
-	key  = "cert/cert.key"
+	path   = "./conf/config.json"
+	config Config
 )
+
+func init() {
+	config = GetConfig(path)
+}
 
 func main() {
 	e := echo.New()
 
 	Middlewares(e)
 	Routes(e)
+	addr := config.Server.Host + ":" + config.Server.Port
 
-	//e.Logger.Fatal(e.StartTLS(addr, cert, key))
-	e.Logger.Fatal(e.Start(addr))
+	if config.UseSSL {
+		e.Logger.Fatal(e.StartTLS(addr, config.Cert, config.Key))
+	} else {
+		e.Logger.Fatal(e.Start(addr))
+	}
+}
+
+type Config struct {
+	ServiceName    string `json:"service_name"`
+	ServiceVersion string `json:"service_version"`
+	Server         struct {
+		Host string `json:"host"`
+		Port string `json:"port"`
+	}
+	UseSSL bool   `json:"use_ssl"`
+	Cert   string `json:"cert"`
+	Key    string `json:"key"`
+}
+
+func GetConfig(path string) Config {
+	var config Config
+
+	file, err := os.Open(path)
+	if err != nil {
+		panic("Error while opening config file (Probably not found)")
+	}
+	defer file.Close()
+
+	byteValue, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic("Error while reading config file")
+	}
+
+	err = json.Unmarshal(byteValue, &config)
+	if err != nil {
+		panic("Error while unmarshalling config file (Probably invalid JSON)")
+	}
+
+	return config
 }
